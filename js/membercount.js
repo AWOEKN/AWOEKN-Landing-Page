95f0f6e1bc7cb4f801164952d78911ee77a93cc6
My Name is Tito <tito@kzg.gg>
2022-01-15 16:28:19 +1300

    updated

--- js/membercount.js
+++ js/membercount.js
@@ -1,14 +1,24 @@
-function getElement(id) {
-    return document.getElementById(id);
-  }
-  
-  fetch('https://discord.com/api/guilds/795552912897736755/widget')
-  .then(res => res.json())
-  .then((res) => {
-    const data = res.data;
-    getElement('name').innerHTML =  'Name: ' + data.name;
-    getElement('symbol').innerHTML = 'Symbol: ' + data.symbol;
-    getElement('rank').innerHTML = 'Rank: ' + data.rank;
-    getElement('price').innerHTML = 'Price: ' + data.quotes.USD.price;
-    // do the rest here
-  });
+                    <?php
+
+                    $json_options = [
+                      "http" => [
+                        "method" => "GET",
+                        "header" => "Authorization: Bot NjQyNzU5Nzc0OTExNTk0NDk2.XcbmsA.Q2HXMbCz1-AbufkBFPzA7r1s0zU"
+                      ]
+                    ];
+                    
+                    $json_context = stream_context_create($json_options);
+                    
+                    $json_get     = file_get_contents('https://discordapp.com/api/guilds/795552912897736755/members?limit=1000', false, $json_context);
+                    
+                    $json_decode  = json_decode($json_get, true);
+                    
+                    echo '<h2>Member Count</h2>';
+                    echo count($json_decode);
+                    
+                    echo '<h2>JSON Output</h2>';
+                    echo '<pre>';
+                    print_r($json_decode);
+                    echo '</pre>';
+                    
+                    ?>
